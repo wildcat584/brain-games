14512106fe59f93615f98a25651ce9843352b20d
Yuriy Shulga <jsshu1ga@gmail.com>
2021-06-12 21:33:22 +0300

    Add greeting the user
    Added readline-sync. cli.js includes the function 'Greeting'

---
+++ bin/src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ', {
+    defaultInput: 'Anonymous'
+  });
+  console.log(`Hello, ${userName}!`);
+};
+
+export default greeting;
